275d55fc842460d49aa9bd03d4611c5742a45402
AleksVasilievich <104069090+AleksVasilievich@users.noreply.github.com>
2022-07-04 12:13:41 +0300

    Add Practice.drawio.png

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿//string[] Array = new string[]{"123","1234","12","2314"};
+ Console.WriteLine("Введите колличество элементовт вашего массива - n -> ");
+ int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите колличество элементовт вашего массива - n -> ");
+
+ Console.WriteLine(n); 
+
+
+
+
